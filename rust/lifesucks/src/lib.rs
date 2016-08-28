ce5e4011e120150e5f09fa92fa486b6e065c7cee
Garrett Berg <gberg@neutral.io>
2016-08-27 21:05:40 -0600

    welll... that works... so I guess go to the next level?

--- rust/lifesucks/src/lib.rs
+++ rust/lifesucks/src/lib.rs
@@ -1,6 +1,7 @@
-
+use std::ops::Deref;
 
 type Result<T> = std::result::Result<T, String>;
+type TryLockResult<T> = std::result::Result<T, String>;
 
 struct Pool<'a> {
     data: &'a [u32],
@@ -11,17 +12,31 @@ struct Mutex<'a> {
     pool: &'a Pool<'a>,
 }
 
-struct MutextGuard<'a> {
+struct MutexGuard<'a> {
     __lock: &'a Mutex<'a>,
 }
 
 impl <'pool> Pool<'pool> {
-    pub fn alloc(&self, i: usize) -> Result<Mutex<'pool> {
-        // Note: if already allocated, return Error
+    pub fn alloc(&'pool self, i: usize) -> Result<Mutex<'pool>> {
+        // Note: in real-life this will fail if not enough mem
         Ok(Mutex{index: i, pool: self})
     }
 }
 
+impl<'pool> Mutex<'pool> {
+    pub fn try_lock(&'pool self) -> TryLockResult<MutexGuard> {
+        // Note: in real-life this will fail if the item is in use
+        Ok(MutexGuard{__lock: self})
+    }
+}
+
+impl<'mutex> Deref for MutexGuard<'mutex> {
+    type Target = u32;
+
+    fn deref(&self) -> &u32 {
+        &self.__lock.pool.data[self.__lock.index]
+    }
+}
 
 #[test]
 fn it_works() {
@@ -29,5 +44,5 @@ fn it_works() {
     let pool = Pool {
         data: &data[..],
     };
-    // assert_eq!(pool.alloc(0).unwrap(), &10);
+    assert_eq!(pool.alloc(0).unwrap().try_lock().unwrap().deref(), &10);
 }
