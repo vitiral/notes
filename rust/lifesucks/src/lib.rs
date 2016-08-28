afca1d9253fde0bd6b794fba5960d98f3459b9aa
Garrett Berg <gberg@neutral.io>
2016-08-27 20:56:27 -0600

    rust life sucks

---
+++ rust/lifesucks/src/lib.rs
@@ -0,0 +1,32 @@
+
+
+type Result<T> = std::result::Result<T, String>;
+
+struct Pool<'a> {
+    data: &'a [u32],
+}
+
+struct Mutex<'a> {
+    index: usize,
+    pool: &'a Pool<'a>,
+}
+
+struct MutextGuard<'a> {
+    __lock: &'a Mutex<'a>,
+}
+
+impl <'pool> Pool<'pool> {
+    pub fn alloc(&self, i: usize) -> Result<&'pool u32> {
+        Ok(&self.data[i])
+    }
+}
+
+
+#[test]
+fn it_works() {
+    let data: [u32; 20] = [10; 20];
+    let pool = Pool {
+        data: &data[..],
+    };
+    assert_eq!(pool.alloc(0).unwrap(), &10);
+}
