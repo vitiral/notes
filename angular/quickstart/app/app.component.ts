73df52c8760822574b4897ec41543acfd044a499
Garrett Berg <vitiral@gmail.com>
2016-11-03 20:58:46 -0600

    cloned quickstart

--- angular/quickstart/app/app.component.ts
+++ angular/quickstart/app/app.component.ts
@@ -1,9 +1,7 @@
-
 import { Component } from '@angular/core';
-
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App -- yay!</h1>',
+    template: `<h1>My First Angular App</h1>`
 })
 export class AppComponent { }
