b42b57e14b668641533aae0cf1b87b8f91bb56f9
Garrett Berg <vitiral@gmail.com>
2016-11-15 17:17:27 -0700

    elm: bear-bones img display

--- elm/tutorial/examples/05-http.elm
+++ elm/tutorial/examples/05-http.elm
@@ -4,8 +4,6 @@ import Html.Events exposing (..)
 import Http
 import Json.Decode as Decode
 
-
-
 main =
   Html.program
     { init = init "cats"
@@ -14,82 +12,34 @@ main =
     , subscriptions = subscriptions
     }
 
-
-
 -- MODEL
-
-
 type alias Model =
-  { topic : String
-  , gifUrl : String
+  { topic: String
+  , gifUrl: String
   }
 
-
 init : String -> (Model, Cmd Msg)
-init topic =
-  ( Model topic "waiting.gif"
-  , getRandomGif topic
-  )
-
-
+init topic = (Model topic "waiting.gif", Cmd.none)
 
 -- UPDATE
 
-
-type Msg
-  = MorePlease
-  | NewGif (Result Http.Error String)
-
+type Msg =
+  Moar
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    MorePlease ->
-      (model, getRandomGif model.topic)
-
-    NewGif (Ok newUrl) ->
-      (Model model.topic newUrl, Cmd.none)
-
-    NewGif (Err _) ->
-      (model, Cmd.none)
-
-
+  (model, Cmd.none)
 
 -- VIEW
-
 
 view : Model -> Html Msg
 view model =
   div []
-    [ h2 [] [text model.topic]
-    , button [ onClick MorePlease ] [ text "More Please!" ]
-    , br [] []
-    , img [src model.gifUrl] []
+    [ h2 [] [ text model.topic ]
+    , img [ src model.gifUrl ] []
+    , button [ onClick Moar ] [ text "Moar Plz" ]
     ]
-
-
-
--- SUBSCRIPTIONS
-
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
-
-
-
--- HTTP
-
-
-getRandomGif : String -> Cmd Msg
-getRandomGif topic =
-  let
-    url =
-      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
-  in
-    Http.send NewGif (Http.get url decodeGifUrl)
-
-
-decodeGifUrl : Decode.Decoder String
-decodeGifUrl =
-  Decode.at ["data", "image_url"] Decode.string
