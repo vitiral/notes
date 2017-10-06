8dfafce224b12c5b532c6e5d17515061769f640d
Garrett Berg <googberg@gmail.com>
2017-10-06 11:36:45 -0600

    git ignore

--- cpp/start/small.cpp
+++ cpp/start/small.cpp
@@ -8,13 +8,22 @@ int main() {
 
     // read the name
     std::string name;  // define name
+    // note: confusingly this ONLY reads a single word
     std::cin >> name;  // read into
+
+    std::string greeting = "Hello, " + name + "!";
+    const std::string spaces(greeting.size(), ' ');
+    const std::string inside = "* " + spaces + " *";
+
+    const std::string outside(inside.size(), '*');
 
     // write a greeting
     std::cout
-        << "Hello, "
-        << name
-        << "!"
-        << std::endl;
+        << std::endl
+        << outside << std::endl
+        << inside << std::endl
+        << "* " << greeting << " *" << std::endl
+        << inside << std::endl
+        << outside << std::endl;
     return 0;
 }
