29507e11c2c6443345d633e2c56ec77ebed66ad1
Garrett Berg <googberg@gmail.com>
2017-10-06 11:24:13 -0600

    hello name

--- cpp/start/small.cpp
+++ cpp/start/small.cpp
@@ -1,9 +1,20 @@
 // a small c++ program
 #include <iostream>
+#include <string>
 
 int main() {
+    // ask for a person's name
+    std::cout << "Please enter your first name: ";
+
+    // read the name
+    std::string name;  // define name
+    std::cin >> name;  // read into
+
+    // write a greeting
     std::cout
-        << "Hello world!"
+        << "Hello, "
+        << name
+        << "!"
         << std::endl;
     return 0;
 }
