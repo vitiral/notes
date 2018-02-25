a890f7d25901ba3ff9a764d902c2e57968852d25
Garrett Berg <googberg@gmail.com>
2018-02-25 12:50:46 -0700

    things work

--- nix/gnu-hello/hello.nix
+++ nix/gnu-hello/hello.nix
@@ -3,9 +3,8 @@ derivation {
   name = "hello";
   builder = "${bash}/bin/bash";
   args = [ ./hello_builder.sh ];
-  inherit gnutar gzip gnumake gcc binutils-unwrapped coreutils gawk gnused gnugrep binutils;
+  inherit gnutar gzip gnumake gcc coreutils gawk gnused gnugrep;
   bintools = binutils.bintools;
   src = ./hello-2.10.tar.gz;
   system = builtins.currentSystem;
 }
-
