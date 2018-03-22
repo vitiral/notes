25e471fb87eacdbcccc068d6b29e4c7fa237289a
Garrett Berg <bgarrett@netapp.com>
2018-03-22 11:39:25 -0600

    add force_repr

---
+++ python/force_repr.py
@@ -0,0 +1,17 @@
+
+def repr_single(s):
+    return "'" + repr('"' + s)[2:]
+
+def repr_double(s):
+    single = repr_single(s)
+    return '"' + single[1:-1].replace('"', '\\"').replace('\\\'', '\'') + '"'
+
+def test_single():
+    assert r"'foobar'" == repr_single('foobar')
+    assert r"'\'foobar'" == repr_single('\'foobar')
+    assert "'\\'foobar'" == repr_single("'foobar")
+
+def test_double():
+    assert r'"foobar"' == repr_double("foobar")
+    assert '"\'foobar"' == repr_double("'foobar")
+    assert '"\\"foobar"' == repr_double('"foobar')
