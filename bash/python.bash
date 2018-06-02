af2b78179aedec614530b5690925494fb1aee4b9
Garrett Berg <vitiral@gmail.com>
2018-06-01 23:19:59 -0600

    add python.bash

--- bash/python.bash
+++ bash/python.bash
@@ -1,3 +1,50 @@
 
-runpy = \
-"import re\n \
+
+function hello_py {
+python3 - <<EOF
+import os
+print("hello cwd:", os.getcwd())
+EOF
+}
+
+function hello_args {
+# NOTE: the argv version actually works (yay!)
+echo "bash-args: $@"
+
+PY_ARGS="$@" python3 - "$@" <<EOF
+import os, sys, shlex
+py_args = os.environ['PY_ARGS']
+print('py_args={}'.format(repr(py_args)))
+
+args = shlex.split(py_args)
+print("args={}".format(repr(args)))
+print("argv={}".format(sys.argv))
+EOF
+}
+
+function py_bash_template {
+python3 - "$@" <<EOF
+#!/usr/bin/python3
+import argparse,os,sys,subprocess as sp
+parser = argparse.ArgumentParser(
+    prog='template',
+    description='example template',
+)
+parser.add_argument('paths', nargs='+', help='paths')
+parser.add_argument('-f', '--flag', action='store_true')
+args = parser.parse_args()
+print('paths={}, flag={}'.format(args.paths, args.flag))
+EOF
+}
+
+hello_py
+hello_args foo bar -a b -z 'foo bar is zzzz'
+
+printf "\nTEMPLATE no flag\n"
+py_bash_template path1 path2
+
+printf "\nTEMPLATE empty flag\n"
+py_bash_template --force
+
+printf "\nTEMPLATE help\n"
+py_bash_template --help
