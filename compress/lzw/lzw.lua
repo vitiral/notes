afbef13e04a1a534c2fd94001a2da8fa21ce5ee6
Rett Berg <vitiral@gmail.com>
2023-10-20 15:59:35 -0500

    ready to test LLZ

--- compress/lzw/lzw.lua
+++ compress/lzw/lzw.lua
@@ -40,15 +40,106 @@ local function tofile(f, mode)
   return f
 end
 
-function M.encodeTh(fn, state, idx, max)
+function M.encodeLzwFull(fn, state, idx, max, dict, size)
+  local build = ''
+  for _, c in fn, state, idx do
+    size = size + 1
+    if size % 1024 == 0 then
+      dpnt(sfmt('Encoded %sKiB', size // 1024))
+    end
+    local new = build..c
+    if dict[new] then build = new
+    else
+      if #build > 0 then
+        co.yield(assert(dict[build])); build = ''
+      end
+      co.yield(string.byte(c))
+    end
+  end
+
+  if #build > 0 then
+    co.yield(assert(dict[build]))
+  end
+end
+
+local function llpop(prev, nxt, i)
+  local pi = prev[i]
+  local ni = nxt[i]
+  nxt[pi] = ni
+  prev[ni] = pi
+  return i
+end
+
+-- insert after
+local function llPutNext(prev, nxt, at, i)
+  local ni = nxt[at]
+  nxt[at] = i
+  prev[i] = at
+
+  nxt[i]   = ni
+  prev[ni] = i
+end
+
+local function llzLLs(max)
+  local llprev, llnext = {0}, {0}
+
+  for i=256,max do
+    llprev[i]=i-1
+    llnext[i]=i+1
+  end
+  llprev[256], llnext[max] = 0, 0
+  return llprev, llnext
+end
+
+
+function M.encodeLlzFull(fn, state, idx, max, dict, size)
+  local llprev, llnext = llzLLs(max)
+  local root = 0 -- root index. Items after root stay, items before root are deleted
+
+  for i, c in fn, state, idx do
+    size = size + 1; if size % 1024 == 0 then
+      dpnt(sfmt('Encoded %sKiB', size // 1024))
+    end
+    assert(type(c) == 'string')
+    dwrite('!! ', c, ': ')
+    local new = build..c
+    local code = dict[new]
+    if code then -- recognized code
+      if code > 255 then
+        llpop(llprev, llnext, code)
+        llPutNext(llprev, llnext, root, code)
+        root = code -- additional recognized items go after this
+      end
+      build = new
+      dpnt(sfmt('building %q', new))
+    else -- Create new code using least-used code.
+      assert(#build > 1)
+      co.yield(assert(dict[build])); build = ''
+      co.yield(string.byte(c))
+      root = 0
+      code = llprev[root] -- replace least-used code with new
+      llpop(llprev, llnext, code)
+      llPutNext(llprev, llnext, root, code)
+      dpnt(sfmt('new %s=%q del %q', code, new, dict[code]))
+      dict[new] = code
+    end
+  end
+
+  if #build > 0 then
+    co.yield(assert(dict[build]))
+  end
+
+end
+
+function M.encodeTh(fullFn, fn, state, idx, max)
   dpnt('!! encodeTh')
   max = max or 0xFFFF
+  fullFn = fullFn or M.encodeLzwFull
   local dict = {}; for i=0,0xFF do dict[string.char(i)] = i end
   local next_code = 0x100
   local build = ""
   local size = 0
   co.yield()
-  local llv, llnext, llprev = {}, {}, {}
   for i, c in fn, state, idx do
     size = size + 1; if size % 1024 == 0 then
       dpnt(sfmt('Encoded %sKiB', size // 1024))
@@ -71,37 +162,53 @@ function M.encodeTh(fn, state, idx, max)
 
   if next_code > max then -- no new codes
     assert(#build == 0)
-    for _, c in fn, state, idx do
-      size = size + 1
-      if size % 1024 == 0 then
-        dpnt(sfmt('Encoded %sKiB', size // 1024))
-      end
-      local new = build..c
-      if dict[new] then build = new
-      else
-        if #build > 0 then
-          co.yield(assert(dict[build])); build = ''
-        end
-        co.yield(string.byte(c))
-      end
-    end
+    fullFn(fn, state, idx, max, dict, size)
   end
   if #build > 0 then
     co.yield(assert(dict[build]))
   end
-  return t
 end
 
 function M.encode(...)
   local t = {}
-  for c in iterTh(M.encodeTh, ...) do
+  for c in iterTh(M.encodeTh, nil, ...) do
     push(t, c)
   end
   return t
 end
 
-function M.decodeTh(fn, state, idx, max)
+
+function M.decodeLzwFull(fn, state, idx, max, dict)
+  for _, c in fn, state, idx do co.yield(assert(dict[c])) end
+end
+
+function M.decodeLlzFull(fn, state, idx, max, dict)
+  local llprev, llnext = llzLLs(max)
+  local root = 0 -- root index. Items after root stay, items before root are deleted
+  local code = nil
+  for _, c in fn, state, idx do
+    assert(type(c) == 'number')
+    if c <= 0xFF then -- Create new code using least-used code.
+      co.yield(c)
+      root = 0
+      local new = dict[code]..string.char(c)
+      code = llprev[root] -- remove least-used code
+      llpop(llprev, llnext, code)
+      llPutNext(llprev, llnext, root, code)
+      dict[new] = code
+    else
+      co.yield(assert(dict[c]))
+      code = c
+      llpop(llprev, llnext, code)
+      llPutNext(llprev, llnext, root, code)
+      root = code
+    end
+  end
+end
+
+function M.decodeTh(fullFn, fn, state, idx, max)
   max = max or 0xFFFF
+  fullFn = fullFn or M.decodeLzwFull
   dpnt('!! DECODE', fn, state, idx, max)
   local t = {}
   local dict = {}; for i=0,0xFF do dict[i] = string.char(i) end
@@ -119,30 +226,30 @@ function M.decodeTh(fn, state, idx, max)
       assert(c <= 0xFF)
       local new = assert(dict[code])..string.char(c)
       dpnt(sfmt('new and push %s%q', next_code, new))
-      dict[next_code] = new; next_code = next_code + 1
       co.yield(new); code = nil
+      dict[next_code] = new; next_code = next_code + 1
       if next_code > max then idx = i;  break end
     end
   end
   if code then co.yield(dict[code]) end
 
   if next_code > max then
-    for _, c in fn, state, idx do co.yield(assert(dict[c])) end
+    fullFn(fn, state, idx, max, dict)
   end
   return table.concat(t)
 end
 
 function M.decode(...)
   local t = {}
-  for c in iterTh(M.decodeTh, ...) do
+  for c in iterTh(M.decodeTh, nil, ...) do
     push(t, c)
   end
   return table.concat(t, '')
 end
 
-function M.encodeFile(from, to)
+function M.encodeFile(from, to, fullFn)
   from, to = assert(tofile(from, 'rb')), assert(tofile(to, 'wb'))
-  for c in iterTh(M.encodeTh, nextFile, from, 1) do
+  for c in iterTh(M.encodeTh, fullFn, nextFile, from, 1) do
     to:write(string.char(c >> 8), string.char(c & 0xFF))
   end
   to:flush()
@@ -158,10 +265,10 @@ function M.readCodes(file)
   end
 end
 
-function M.decodeFile(from, to)
+function M.decodeFile(from, to, fullFn)
   from, to = assert(tofile(from, 'rb')), assert(tofile(to, 'wb'))
   dpnt('Decoding ', from, M.readCodes, from)
-  for s in iterTh(M.decodeTh, M.readCodes, from) do
+  for s in iterTh(M.decodeTh, fullFn, M.readCodes, from) do
     to:write(s)
   end
   to:flush()
